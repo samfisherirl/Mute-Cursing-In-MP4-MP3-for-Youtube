8e5e3022304eaa58d67b440702d502e794a6eee7
Sam A <98753696+samfisherirl@users.noreply.github.com>
2024-02-17 11:38:45 -0500

    Add files via upload

---
+++ one-click-installer.bat
@@ -0,0 +1,28 @@
+@echo off
+SET venv_dir=venv
+
+REM Check if the virtual environment directory exists
+IF EXIST "%venv_dir%\Scripts\activate.bat" (
+    ECHO Virtual environment found. Activating...
+) ELSE (
+    ECHO Creating virtual environment...
+    python -m venv %venv_dir%
+)
+
+REM Activate the virtual environment
+CALL %venv_dir%\Scripts\activate.bat
+
+REM Upgrade pip and install requirements if needed
+IF NOT EXIST "%venv_dir%\Scripts\pip.exe" (
+    ECHO Installing dependencies...
+    python -m pip install --upgrade pip
+    python -m pip install --upgrade -r requirements.txt
+)
+
+REM Run the Python script
+python run_convert_transcript_censor.py
+
+REM close this window on completion
+
+REM Pause the command window
+pause
