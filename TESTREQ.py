c492c45724fc94bfa2aacd2c370a4774908e9188
Sam A <98753696+samfisherirl@users.noreply.github.com>
2024-03-25 23:14:54 -0400

    WELLY WEAN

--- TESTREQ.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url =  r"https://undelete.pullpush.io/r/LivestreamFail/comments/1azuqpe/forsen_agrees_with_hasans_take/"
-
-
-print(requests.get(url, allow_redirects=True, stream=True).text)
--- test.py
+++
@@ -1,3 +0,0 @@
-
-transcript_file = load_transcript()
-input_video_path = select_audio_or_video()  
